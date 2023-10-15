fb8e86d18109746a7646933fac95b6c70635ac9e
sam hockaday <samuel.hockaday@gmail.com>
2023-10-15 18:48:38 +0100

    JS function and DOM

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
- // Get a reference to the button element by its ID
- let btn = document.querySelector(".btn2");
- let luckNumber = document.querySelector(".luckyNumber");
+//  // Get a reference to the button element by its ID
+//  let btn = document.querySelector(".btn2");
+//  let luckNumber = document.querySelector(".luckyNumber");
 
- // Add a click event listener to the button
- btn.addEventListener("click", function () {
-     // Generate a random number between 1 and 20
-     let randomNumber = Math.floor(Math.random() * 20) + 1;
+//  // Add a click event listener to the button
+//  btn.addEventListener("click", function () {
+//      // Generate a random number between 1 and 20
+//      let randomNumber = Math.floor(Math.random() * 20) + 1;
 
-     // Display the random number
-     luckyNumber.textContent = "Random Number: " + randomNumber;
- });
- console.log("helloworld");
+//      // Display the random number
+//      luckyNumber.textContent = "Random Number: " + randomNumber;
+//  });
+//  console.log("helloworld");
+
+
+
+
+document.querySelector('.luckyNumber').textContent = 'Between 1-20 🐹';
+
+document.querySelector('.btn2').addEventListener('click', function(){
+    
+});
